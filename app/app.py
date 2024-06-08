cfc5c2c34160b0bf2a833fde202554c51552adbb
brunaogomes11 <brunogomesper@gmail.com>
2024-06-08 17:33:56 -0300

    mudança para api

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, redirect, render_template, request, send_from_directory, current_app, g, jsonify
+from flask import Flask, redirect, render_template, request, send_from_directory, current_app, g, jsonify, send_file
 import os
 import configparser
 from pymongo import MongoClient, InsertOne
+from bson import ObjectId, Binary
+import tempfile
 
 app = Flask(__name__)
 # Atualizando para buscar a URI corretamente
@@ -11,25 +13,44 @@ db = client.mydata
 collection = db.mytable
 @app.route("/")
 def homePage():
-    return render_template("index.html")
+    return "Servidor Ok"
 
-@app.route("/insercao")
-def insercao():
-    return render_template("insercao.html")
+@app.route("/inserir")
+def inserir():
+    file = request.files['file']
+    other_data = request.form.to_dict()
+    id_objeto = ObjectId()
+    try:
+        filename = file.filename
+        file_data = Binary(file.read())
+        # Insere o arquivo no MongoDB
+        document = {'_id':id_objeto, 'filename': filename, 'file_data': file_data}
+        # Adiciona outros dados ao documento
+        document.update(other_data)
+        collection.insert_one(document)
+        return jsonify({'message': 'Arquivo e dados guardados'})
+    except:
+        return jsonify({'message': 'Arquivo não encontrado'})
 
-@app.route("/listagem", defaults={'lista_db':''})
-def listagem(lista_db):
-    if (lista_db != ''):
-        return render_template('listagem.html', documents=lista_db)
-    else:
-        mydata_collection = collection.find()
-        return render_template("listagem.html", documents=mydata_collection)
+@app.route("/listar")
+def listar():
+    mydata_collection = collection.find()
+    try:
+        result = []
+        for data in mydata_collection:
+            data['_id'] = str(data['_id'])
+            if ('file_data' in data):
+                del data['file_data']
+            result.append(data)
+        return jsonify(result)
+    except:
+        jsonify({"error":"Não foi possível encontrar nenhum dado"})
 
-@app.route("/busca", methods=["GET","POST"])
+@app.route("/buscar", methods=["GET", "POST"])
 def busca():
-    if (request.method == "POST"):
+    try:
         query = request.form.get("search-bar")
-        if(query):
+        if query:
             index_config = {
                 "$search": {
                     "index": "teste-search",
@@ -42,9 +63,35 @@ def busca():
                 }
             }
             results = collection.aggregate([index_config])
-            return listagem(results)
+            formatted_results = []
+            for result in results:
+                result['_id'] = str(result['_id'])
+                del result['file_data']
+                formatted_results.append(result)
+            print(formatted_results)
+            return jsonify(formatted_results)
         else:
-            return 404
+            return jsonify({"error": "Formulário Inválido"}), 400
+    except:
+        return jsonify({"error": "Método não permitido"}), 405
 
+@app.route("/baixar", methods=["GET", "POST"])
+def baixar():
+    id_objeto = request.form.get("_id")
+    file_data = collection.find_one({"_id": ObjectId(id_objeto)})
+    try:
+        # Obter os dados binários
+        binary_data = file_data.get('file_data')
+        nome = file_data.get('filename')
+        # Criar um novo arquivo PDF
+        with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as temp_pdf_file:
+            temp_pdf_path = temp_pdf_file.name
+            temp_pdf_file.write(binary_data)
+
+        # Enviar o PDF como resposta
+        return send_file(temp_pdf_path, as_attachment=True)
+    except:
+        return "Arquivo não encontrado", 404
+        
 if __name__ == '__main__':
     app.run(debug=True)
